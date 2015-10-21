b6d9d852eee7e711a845ff5ea2d2f060b3e05456
Dca <dca.hsu@gmail.com>
2015-10-21 15:15:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,27 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install curl git git-flow iftop tree ncdu
 brew install caskroom/cask/brew-cask
 
+# 更好的終端機
 brew cask install iterm2
 
+# 瀏覽器
 brew cask install google-chrome
-brew cask install 1password
+
+# 雲端同步
 brew cask install dropbox
+
+# 密碼管理
+brew cask install 1password
+
+# 資料庫GUI
 brew cask install jeromelebel-mongohub psequel sequel-pro
+
+# 虛擬化
 brew cask install dockertoolbox
+
+# 音樂
 brew cask install kkbox
+
+# 工具列輔助
+brew cask install bartender
+
